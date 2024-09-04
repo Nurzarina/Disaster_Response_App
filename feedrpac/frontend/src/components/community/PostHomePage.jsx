f5ff701c4d79ee778580d2d1da2c32859057c522
yatt-code <32289374+yatt-code@users.noreply.github.com>
2024-09-05 06:57:56 +0800

    semi-fix community page, convert to bootstrap

--- feedrpac/frontend/src/components/community/PostHomePage.jsx
+++ feedrpac/frontend/src/components/community/PostHomePage.jsx
@@ -1,46 +1,23 @@
-import { useState } from "react";
-import Posts from "../../components/utils/common/Posts";
-import CreatePost from "./CreatePost";
-import { Container } from "react-bootstrap";
+import { useState } from 'react';
+import Posts from '../utils/common/Posts';
+import CreatePost from './CreatePost';
 import './PostHomePage.css';
 
 const PostHomePage = () => {
-	const [feedType, setFeedType] = useState("forYou");
+    const [feedType, setFeedType] = useState('forYou');
 
-	return (
-		<Container id="post-container">
-			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
-				{/* Header */}
-				<div className='flex w-full border-b border-gray-700'>
-					<div
-						className={
-							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-						}
-						onClick={() => setFeedType("forYou")}
-					>
-						For you
-						{feedType === "forYou" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-					<div
-						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
-						onClick={() => setFeedType("following")}
-					>
-						Following
-						{feedType === "following" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-				</div>
-
-				{/*  CREATE POST INPUT */}
-				<CreatePost />
-
-				{/* POSTS */}
-				<Posts />
-			</div>
-		 </Container>
-	);
+    return (
+        <div className='post-container'>
+            <div className='post-header'>
+                <div className='nav-tabs'>
+                    <a className={`nav-link ${feedType === 'forYou' ? 'active' : ''}`} onClick={() => setFeedType('forYou')}>For you</a>
+                    <a className={`nav-link ${feedType === 'following' ? 'active' : ''}`} onClick={() => setFeedType('following')}>Following</a>
+                </div>
+            </div>
+            <CreatePost />
+            <Posts feedType={feedType} />
+        </div>
+    );
 };
+
 export default PostHomePage;
