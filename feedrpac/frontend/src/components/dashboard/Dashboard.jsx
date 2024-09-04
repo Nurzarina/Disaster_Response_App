487f497db62a0241a6a66c1341999bf2c479eae8
Nurzarina <nurzarina_os@yahoo.com>
2024-09-04 21:41:03 +0800

    Added title to Dashboard

--- feedrpac/frontend/src/components/dashboard/Dashboard.jsx
+++ feedrpac/frontend/src/components/dashboard/Dashboard.jsx
@@ -40,68 +40,86 @@ const Dashboard = () => {
         }));
 
     return (
-        <Container fluid id="dashboard-container">
-            <Row className="justify-content-center my-4">
+        <>
+            {/* Page Title */}
+            <Row className="justify-content-center mb-4">
                 <Col xs={12} md={12} lg={12} className="text-center">
-                    <CSSTransition
-                        in={!!user}
-                        timeout={500}
-                        classNames="fade"
-                        unmountOnExit
-                    >
-                        <div id="userArea" className="ml-3">
-                            {user ? (
-                                 // User Profile Card
-                                 <UserCard />
-                            ) : (
-                                <></>  // Render empty JSX to avoid passing `null`
-                            )}
-                        </div>
-                    </CSSTransition>
-                    <CSSTransition
-                        in={!user}
-                        timeout={500}
-                        classNames="fade"
-                        unmountOnExit
-                    >
-                        <div id="guestArea">
-                            {!user ? (
-                                 // 'Login' & 'SignUp' Call-to-action button
-                                 <LogInCard />
-                            ) : (
-                                <></>  // Render empty JSX to avoid passing `null`                            
+                    <h1 className="display-4"><b>Welcome to Disaster Response App</b></h1>
+                    <p className="lead" style={{
+                        color: '#FFFFFF',
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)', /* Dark semi-transparent background */
+                        padding: '0.5rem',
+                        borderRadius: '5px'
+                    }}>
+                        Stay informed about ongoing and recent disasters.
+                    </p>
+                </Col>
+            </Row>
+            {/* Content */}
+            <Container fluid id="dashboard-container">
+                {/* User Login Area */}
+                <Row className="justify-content-center my-4">
+                    <Col xs={12} md={12} lg={12} className="text-center">
+                        <CSSTransition
+                            in={!!user}
+                            timeout={500}
+                            classNames="fade"
+                            unmountOnExit
+                        >
+                            <div id="userArea" className="ml-3">
+                                {user ? (
+                                    // User Profile Card
+                                    <UserCard />
+                                ) : (
+                                    <></>  // Render empty JSX to avoid passing `null`
                                 )}
-                        </div>
-                    </CSSTransition>
-                </Col>
-            </Row>
-
-            <Row id="ReportWidgetContainer" className="justify-content-center">
-                <Col xs={12} md={10} lg={8} id="ReportWidgetRow">
-                    <ReportWidget />
-                </Col>
-            </Row>
-
-            <Row className="justify-content-center" id="EventsMapRow">
-                <Col xs={12} md={12} lg={8} id="EventsMapCol">
-                    <CSSTransition
-                        in={showMap}
-                        timeout={700}
-                        classNames="fade"
-                        unmountOnExit
-                    >
-                        <Card id="EventsMapContainer">
-                            <Card.Body>
-                                <Card.Title id="EventsMapTitle" className="text-center">
-                                    Events within Malaysia
-                                </Card.Title>
-                                <EventsMap events={events} />
-                            </Card.Body>
-                        </Card>
-                    </CSSTransition>
-                </Col>
-            </Row>
-        </Container>
+                            </div>
+                        </CSSTransition>
+                        <CSSTransition
+                            in={!user}
+                            timeout={500}
+                            classNames="fade"
+                            unmountOnExit
+                        >
+                            <div id="guestArea">
+                                {!user ? (
+                                    // 'Login' & 'SignUp' Call-to-action button
+                                    <LogInCard />
+                                ) : (
+                                    <></>  // Render empty JSX to avoid passing `null`                            
+                                )}
+                            </div>
+                        </CSSTransition>
+                    </Col>
+                </Row>
+                {/* Report Widget area */}
+                <Row id="ReportWidgetContainer" className="justify-content-center">
+                    <Col xs={12} md={10} lg={8} id="ReportWidgetRow">
+                        <ReportWidget />
+                    </Col>
+                </Row>
+                {/* Overall Situation area */}
+                <Row className="justify-content-center" id="EventsMapRow">
+                    <Col xs={12} md={12} lg={8} id="EventsMapCol">
+                        <CSSTransition
+                            in={showMap}
+                            timeout={700}
+                            classNames="fade"
+                            unmountOnExit
+                        >
+                            <Card id="EventsMapContainer">
+                                <Card.Body>
+                                    <Card.Title id="EventsMapTitle" className="text-center">
+                                        Events within Malaysia
+                                    </Card.Title>
+                                    <EventsMap events={events} />
+                                </Card.Body>
+                            </Card>
+                        </CSSTransition>
+                    </Col>
+                </Row>
+            </Container >
+        </>
     );
 };
 
